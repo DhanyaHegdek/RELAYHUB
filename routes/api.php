7711f9ba95caf2117ac4467e8f3952d097e3966f
dhanyashreemh <dhanyashree@raakacloud.com>
2026-05-21 10:26:47 +0530

    added routes for chat

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
 
 //instead of default sanctum auth, we will use JWT auth for API routes
 Route::middleware('auth:api')->get('/user', function (Request $request) {
@@ -19,3 +20,10 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/me',      [AuthController::class, 'me']);
 }); 
 
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/conversations', [ChatController::class, 'startConversation']);
+    Route::get('/conversations', [ChatController::class, 'getConversations']);
+    Route::get('/conversations/{id}/messages', [ChatController::class, 'getMessages']);
+    Route::post('/conversations/{id}/messages', [ChatController::class, 'sendMessage']);
+});
