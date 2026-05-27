0614862b2931bf9ef7fa4066cfe5a7dced07f20d
dhanyashreemh <dhanyashree@raakacloud.com>
2026-05-27 15:36:05 +0530

    updated user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,18 +13,13 @@ class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles;
 
-    /**
-     * Mass assignable attributes
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'bio',
     ];
 
-    /**
-     * Hidden attributes
-     */
     protected $hidden = [
         'password',
         'remember_token',
@@ -32,40 +27,30 @@ class User extends Authenticatable implements JWTSubject
         'two_factor_recovery_codes',
     ];
 
-    /**
-     * Attribute casting
-     */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-        'two_factor_confirmed_at' => 'datetime',
+        'email_verified_at'      => 'datetime',
+        'password'               => 'hashed',
+        'two_factor_confirmed_at'=> 'datetime',
     ];
 
-    /**
-     * JWT Identifier
-     */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
-    /**
-     * Custom JWT Claims
-     */
     public function getJWTCustomClaims()
     {
         return [];
     }
 
-     // User has many conversations
-    public function conversations() {
+    public function conversations()
+    {
         return $this->hasMany(Conversation::class, 'user_one_id')
             ->orWhere('user_two_id', $this->id);
     }
 
-    // User has many messages sent
-    public function messages() {
+    public function messages()
+    {
         return $this->hasMany(Message::class, 'sender_id');
     }
-
 }
