de6186bdab9a0acfe98b7738018af865692d5230
dhanyashreemh <dhanyashree@raakacloud.com>
2026-06-01 11:56:21 +0530

    added route to storageinfo

--- routes/api.php
+++ routes/api.php
@@ -59,4 +59,6 @@ Route::middleware('auth:api')->group(function () {
 
     Route::get('/conversations/{id}/files', [ChatController::class, 'getFiles']);
 
+    Route::get('/storage-info', [ChatController::class, 'storageInfo']);
+
 });
